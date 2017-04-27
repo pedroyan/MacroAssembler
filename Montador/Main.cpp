2e848ddd600dcc1d84156ca7933f38f48dfe241c
Pedro Yan Ornelas de Oliveira <pedroyane@gmail.com>
2017-04-27 12:48:34 -0300

    Separadas diretivas de pré-compilação de release e debug

--- Montador/Main.cpp
+++ Montador/Main.cpp
@@ -6,11 +6,19 @@
 using std::string;
 
 int main(int argc, char *argv[]) {
+
+	#ifdef _DEBUG
+	//seta programaticamente os argumentos em modo debug
+	argv[1] = "-p";
+	#else
+	//pega os argumentos da linha de comando em modo release
 	if (argc < 4) {
 		printf("Sao necessarios 3 argumentos para executar o montador:\n");
 		printf("cmd>Montador <Tipo de Operacao> <Arquivo de Entrada> <Arquivo de Saida>\n");
 		return 0;
 	}
+	#endif // DEBUG
+
 	string tipoOperacao = StringLibrary::ToLower(argv[1]);
 
 	if (tipoOperacao == "-p") {
@@ -21,5 +29,6 @@ int main(int argc, char *argv[]) {
 		printf("tipo operação não especificado");
 	}
 
+	getchar();
 	return 0;
 }
