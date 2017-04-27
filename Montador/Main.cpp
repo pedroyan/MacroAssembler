9e9e9c7c98f399700b9cd6721ac8520b92600051
Pedro Yan Ornelas de Oliveira <pedroyane@gmail.com>
2017-04-27 12:37:12 -0300

    Utilizada shared library

--- Montador/Main.cpp
+++ Montador/Main.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
+#include "MacroAssemblerLibraries.h"
 
 using std::string;
 
@@ -8,11 +9,17 @@ int main(int argc, char *argv[]) {
 	if (argc < 4) {
 		printf("Sao necessarios 3 argumentos para executar o montador:\n");
 		printf("cmd>Montador <Tipo de Operacao> <Arquivo de Entrada> <Arquivo de Saida>\n");
+		return 0;
 	}
-	string tipoOperacao(argv[1]);
+	string tipoOperacao = StringLibrary::ToLower(argv[1]);
 
 	if (tipoOperacao == "-p") {
-
+		printf("pre-processamento");
+	} else if (tipoOperacao == "-o") {
+		printf("montagem");
+	} else {
+		printf("tipo operação não especificado");
 	}
+
 	return 0;
 }
