1430d3d277b6b13e8596d4a8ab201f69b6afcd31
Pedro Yan Ornelas de Oliveira <pedroyane@gmail.com>
2017-05-05 15:10:48 -0300

    Criado fileLibrary

--- Montador/Main.cpp
+++ Montador/Main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <fstream>
 #include "MacroAssemblerLibraries.h"
+#include "FileLibrary.h"
 #include "PreProcessor.h"
 
 using std::string;
@@ -28,21 +29,24 @@ int main(int argc, char *argv[]) {
 
 	string tipoOperacao = StringLibrary::ToLower(argv[1]);
 
-	ifstream& fileStream = VerifyFile(argv[2]);
+	ifstream fileStream;
 
-	if (tipoOperacao == "-p") {
-		PreProcessor processor(argv[2],argv[3]);
-		processor.PreProcessPass(fileStream);
-		fileStream.close();
-	} else if (tipoOperacao == "-o") {
-		PreProcessor processor(argv[2], argv[3]);
-		if (processor.PreProcessPass(fileStream)) {
+	if (FileLibrary::VerifyFile(argv[2], "asm","O Montador aceita somente arquivos .asm ",&fileStream)) {
+		if (tipoOperacao == "-p") {
+			PreProcessor processor(argv[2], argv[3]);
+			processor.PreProcessPass(fileStream);
+		} else if (tipoOperacao == "-o") {
+			PreProcessor processor(argv[2], argv[2]);
+			if (processor.PreProcessPass(fileStream)) {
 
+			}
+		} else {
+			printf("tipo de operacao nao especificada");
 		}
-	} else {
-		printf("tipo operação não especificado");
 	}
 
+	fileStream.close();
+	printf("\nPressione enter para fechar");
 	getchar();
 	return 0;
 }
