3c63e1a540c8c02b72a947e743b114d6c29a7d45
Pedro Yan Ornelas de Oliveira <pedroyane@gmail.com>
2017-05-06 22:00:56 -0300

    Concluida tabela de instrucoes

--- Montador/TableManager.cpp
+++ Montador/TableManager.cpp
@@ -4,6 +4,18 @@
 unordered_map<string, InstructionInfo> TableManager::InstructionTable{
 	{"add", {1,1} },
 	{"sub", {1,2} },
+	{"mult",{1,3} },
+	{"div", {1,4} },
+	{"jmp", {1,5} },
+	{"jmpn", {1,6}},
+	{"jmpp", {1,7}},
+	{"jmpz", {1,8}},
+	{"copy", {2,9}},
+	{"load", {1,10}},
+	{"store", {1,11}},
+	{"input", {1,12}},
+	{"output", {1,13}},
+	{"stop", {0, 14}}
 };
 
 InstructionInfo * TableManager::GetInstruction(string name) {
