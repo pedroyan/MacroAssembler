feda0987bb71db7d15c81eb69700e28f359a9d58
Pietro Bertarini <pietromota@hotmail.com>
2017-05-13 18:38:11 -0300

    Arrumado os casos em que o arquivo objeto possui spaces e enter

--- Ligador/ModuleEngine.cpp
+++ Ligador/ModuleEngine.cpp
@@ -55,24 +55,34 @@ void ModuleEngine::ReadFile() { // lê as informações  do aqruivo .o
 	vector<ObjectTable> tableUse, tableDefinition;
 	vector<ObjectCode> objectCode;
 	auto line = leitor.GetNextLine(fp);
-	if (StringLibrary::ToLower(line) == string("table use")) {
+	StringLibrary::Tokenize(line, " ", members);
+	if (StringLibrary::ToLower(members[1]) == string("use")) {
 		do {
 			members.clear();
 			line = leitor.GetNextLine(fp);
 			StringLibrary::Tokenize(line, " ", members);
+			if (members.size() == 0) {
+				line = leitor.JumpForNextLine(members, fp);
+					StringLibrary::Tokenize(line, " ", members);
+			}
 			if (StringLibrary::ToLower(members[0]) != string("table")) {
 				tableUse.push_back(ObjectTable(members[0], atoi(members[1].c_str())));
 			}
 
 
 		} while (StringLibrary::ToLower(members[0]) != string("table"));
-		 SetTableUse(tableUse);
+		SetTableUse(tableUse);
 	}
-	if (StringLibrary::ToLower(line) == string("table definition")) {
+	StringLibrary::Tokenize(line, " ", members);
+	if (StringLibrary::ToLower(members[1]) == string("definition")) {
 		do {
 			members.clear();
 			line = leitor.GetNextLine(fp);
 			StringLibrary::Tokenize(line, " ", members);
+			if (members.size() == 0) {
+				line = leitor.JumpForNextLine(members, fp);
+				StringLibrary::Tokenize(line, " ", members);
+			}
 			if (StringLibrary::ToLower(members[0]) != string("table")) {
 				tableDefinition.push_back(ObjectTable(members[0], atoi(members[1].c_str())));
 			}
@@ -80,11 +90,16 @@ void ModuleEngine::ReadFile() { // lê as informações  do aqruivo .o
 		} while (StringLibrary::ToLower(members[0]) != string("table"));
 		 SetTableDefenition(tableDefinition);
 	}
-	if (StringLibrary::ToLower(line) == string("table realocation")) {
+	StringLibrary::Tokenize(line, " ", members);
+	if (StringLibrary::ToLower(members[1]) == string("realocation")) {
 		do {
 			members.clear();
 			line = leitor.GetNextLine(fp);
 			StringLibrary::Tokenize(line, " ", members);
+			if (members.size() == 0) {
+				line = leitor.JumpForNextLine(members, fp);
+				StringLibrary::Tokenize(line, " ", members);
+			}
 			if (StringLibrary::ToLower(members[0]) != string("code")) {
 				 SetTableRealocation(members[0]);
 			}
